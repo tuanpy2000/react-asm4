1b1a054d3823c84d9d029411c835bfef63d4c3db
Tuan <phamdinhtuan1912@gmail.com>
2022-07-19 22:44:04 +0700

    add code asm4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
+import PrivateRoute from './components/PrivateRoute';
+import AuthRoute from './components/AuthRoute';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<Navigate to='/login' replace />} />
+          <Route exact path='/home' element={<PrivateRoute><Home /></PrivateRoute>} />
+          <Route exact path='/login' element={<AuthRoute><Login /></AuthRoute>} />
+        </Routes>
+      </div>
+
+    );
+  }
 }
 
-export default App;
---
+++ src/components/ToDoForm/index.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import { Link } from "react-router-dom";
+
+export default class ToDoForm extends Component {
+    render() {
+        return (
+
+            <div className='form-container'>
+                <h1>New Task</h1>
+                <button className='btn-signout' onClick={this.props.onLogout}>          <Link to="/login">Sign out</Link></button>
+                <div className='form-input'>
+                    <div className='form-text'>
+                        <label>Name</label>
+                        <input className='name' onChange={this.props.taskName}>
+                        </input>
+                    </div>
+                    <div className='form-date'>
+                        <label>Deadline</label>
+                        <input type='date' className='date' onChange={this.props.taskDeadline}>
+                        </input>
+                    </div>
+                </div>
+                <div className='form-check'>
+                    <div className='check-list'>
+                        <label>Priority:</label>
+                        <div>
+                            <input type='radio' name='priority-check' id='high-check' onChange={this.props.taskPriority} value='high'></input>
+                            <label>High</label>
+                        </div>
+                        <div>
+                            <input type='radio' name='priority-check' id='medium-check' onChange={this.props.taskPriority} value='medium'></input>
+                            <label>Medium</label>
+                        </div>
+                        <div>
+                            <input type='radio' name='priority-check' id='low-check' onChange={this.props.taskPriority} value='low'></input>
+                            <label>Low</label>
+                        </div>
+                    </div>
+                    <button onClick={this.props.onSubmit}>Submit
+                    </button>
+                </div>
+            </div>
+
+        )
+    }
+}
